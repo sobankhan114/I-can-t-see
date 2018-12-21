389999ab1372e36b9c0b06486b40e69897cba983
sobankhan114 <44096380+sobankhan114@users.noreply.github.com>
2018-12-22 03:59:24 +0500

    Update kkjhjhgjgjgjfhfuy.m

--- kkjhjhgjgjgjfhfuy.m
+++ kkjhjhgjgjgjfhfuy.m
@@ -1,13 +1,13 @@
 clear all %#ok<CLALL>
 clc
 
-%{
+
 x=[1,2,3,4,5];
 y=[0,0,0,0,0];
-x1=linspace(1,5,100);
-num=1;
-%}
-
+mdata=zeros(1,5);
+bdata=zeros(1,5);
+error=zeros(1,5);
+yf=[0,0,0,0,0];
 
 
 image1=imread('1.jpg');
@@ -22,66 +22,46 @@ im3 = rgb2gray(image3);
 im4 = rgb2gray(image4);
 im5 = rgb2gray(image5);
 
-im1=imresize(im1,0.2);
-im2=imresize(im2,0.2);
-im3=imresize(im3,0.2);
-im4=imresize(im4,0.2);
-im5=imresize(im5,0.2);
-
-
-
-x=1:8;
-y=[1.69,1.78,1.57,1.59,1.65,1.68,1.70,1.71];
-p1=polyfit(x,y,7);
-x1=linspace(1,5,100);
-f1=polyval(p1,x1);
-p2=diff(p1);
-p3=diff(p1,2);
-p4=diff(p1,3);
-
-%{
-subplot(3,1,1),plot(x1,f1);
-f2=polyval(p2,x1);
-subplot(4,1,2),plot(x1,f2);
-f3=polyval(p3,x1);
-subplot(4,1,3),plot(x1,f3);
-f4=polyval(p4,x1);
-subplot(4,1,4),plot(x1,f4);
-%}
-f1=polyval(p1,x1);
-plot(x1,f1);
-
-
-
-
-%{
-
-subplot(2,3,1), imshow(im1)
-subplot(2,3,2), imshow(im2)
-subplot(2,3,3), imshow(im3)
-subplot(2,3,4), imshow(im4)
-subplot(2,3,5), imshow(im5)
-
-%}
-
+im1=imresize(im1,0.05);
+im2=imresize(im2,0.05);
+im3=imresize(im3,0.05);
+im4=imresize(im4,0.05);
+im5=imresize(im5,0.05);
 
 
 
 imf=cat(3,im1,im2,im3,im4,im5);
 
+[roww,coll,deapthh]=size(imf);
 
-%{
+gfinal=zeros(roww,coll);
 
-for row=106:116
-    for col=405:415
-        for deapth=1:5
-            y(deapth)=imf(row,col,deapth)/10;
+
+for row=1:roww
+    for col=1:coll
+        for deapth=1:deapthh
+            y(deapth)=imf(row,col,deapth);
         end
-        p=polyfit(x,y,4);
-        f1=polyval(p,x1);
-        subplot(10,10,num),plot(x1,f1);
-        num=num+1;
+        yf=y;
+        [r,m,b]=regression(x,y);
+        
+        for i=1:deapthh
+            ym=m*i+b;
+            y(i)=ym;
+            [r,mdata(i),bdata(i)]=regression(x,y);
+            y=yf;
+            error(i)=abs(m-mdata(i))+abs(b-bdata(i));
+        end
+        [val,ind]=max(error);
+        gfinal(row,col)=yf(ind);
     end
 end
 
-%}
+subplot(2,3,1),imshow(im1);
+subplot(2,3,2),imshow(im2);
+subplot(2,3,3),imshow(im3);
+subplot(2,3,4),imshow(im4);
+subplot(2,3,5),imshow(im5);
+subplot(2,3,6),imshow(gfinal,[0,255]);
+
+
